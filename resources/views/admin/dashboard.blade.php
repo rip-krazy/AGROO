34c9925fe08a1193291b09c73e7bf025e9d57039
ilham.desqi <ilhamdesqidev@gmail.com>
2025-07-02 13:25:34 +0700

    nambahin surat pengajuan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -25,7 +25,7 @@
                             Dashboard
                         </a>
                         
-                        <a href="{{ route('admin.stock') }}" class="flex items-center px-4 py-2 text-white rounded-lg hover:bg-indigo-700">
+                         <a href="{{ route('admin.stock') }}" class="flex items-center px-4 py-2 text-white rounded-lg hover:bg-indigo-700">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
                             </svg>
@@ -39,6 +39,13 @@
                             Unit
                         </a>
                         
+                        <a href="#" class="flex items-center px-4 py-2 text-white rounded-lg hover:bg-indigo-700">
+                            <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                            </svg>
+                            Surat Pengajuan
+                        </a>
+
                         <a href="#" class="flex items-center px-4 py-2 text-white rounded-lg hover:bg-indigo-700">
                             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
